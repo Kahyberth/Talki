e79063499f77353051a33463aa5420a980e13d93
Carlos Guerrero <carlosguerrero0705@gmail.com>
2024-12-02 14:57:52 -0500

    fix: Added function to add new server in app

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useState, ReactNode } from "react";
-import { ServerNavigation } from "./server-navigation";
-import { ServerDiscovery } from "./server-discovery";
+import { ServerNavigation } from "./servers/server-navigation";
+import { ServerDiscovery } from "./servers/server-discovery";
+import { CreateServerModal } from "./servers/server-creation";
+import serverlist from "./servers/serverlist.json";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -10,6 +12,7 @@ interface MainLayoutProps {
 
 export function MainLayout({ children }: MainLayoutProps) {
   const [isDiscoveryOpen, setIsDiscoveryOpen] = useState(false);
+  const [isPlusOpen, setIsPlusOpen] = useState(false);
 
   const handleOpenDiscovery = () => {
     setIsDiscoveryOpen(true);
@@ -19,20 +22,40 @@ export function MainLayout({ children }: MainLayoutProps) {
     setIsDiscoveryOpen(false);
   };
 
+  const handleOpenPlus = () => {
+    setIsPlusOpen(true);
+  };
+
+  const handlePlusClose = () => {
+    setIsPlusOpen(false);
+  }
+
   return (
-    <div className="flex h-screen ">
+    <div className="flex h-screen overflow-hidden">
       {/* Barra de navegación lateral */}
-      <ServerNavigation onCompassClick={handleOpenDiscovery} />
+      <ServerNavigation onCompassClick={handleOpenDiscovery} onPlusClick={handleOpenPlus} serverList={serverlist}  />
 
       {/* Contenido principal */}
-      <div className="flex-1 p-4 overflow-auto">{children}</div>
+      <div className="flex-1 flex flex-col">{children}</div>
+
+      {/* Modal para Server Discovery */}
       {isDiscoveryOpen && (
-        <div className="fixed inset-0 z-50 flex items-center overflow-hidden justify-center bg-black bg-opacity-50">
-          <div className="relative">
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="relative w-full max-w-4xl overflow-hidden rounded-lg">
             <ServerDiscovery onClose={handleCloseDiscovery} />
+          </div>
+        </div>
+      )}
+
+      {/* Modal para Create Server */}
+      {isPlusOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="relative w-full max-w-md overflow-hidden rounded-lg">
+            <CreateServerModal onClose={handlePlusClose} onCreate={(serverName) => console.log(serverName)} />
           </div>
         </div>
       )}
     </div>
   );
+
 }
