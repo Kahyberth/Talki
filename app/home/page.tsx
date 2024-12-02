cae0facf53aeb38c21fbd0372388fdb29687e86e
Carlos Guerrero <carlosguerrero0705@gmail.com>
2024-12-02 14:54:04 -0500

    fix: Change layout in page for server

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,8 @@
 // pages/discord-clone.tsx (o donde esté ubicada tu página)
-import { ChannelSidebar } from "@/components/channel-sidebar";
-import { Chat } from "@/components/chat";
-import { MemberList } from "@/components/member-list";
-
+import HomeLayout from "@/components/home/HomeLayout"
 export default async function DiscordClone() {
 
-  
-
   return (
-    <div className="flex h-screen overflow-hidden	 bg-[#313338] text-gray-100">
-      <ChannelSidebar />
-      <Chat />
-      <MemberList />
-    </div>
+    <HomeLayout />
   )
 }
