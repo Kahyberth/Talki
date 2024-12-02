260a7909ac776248db7470fcbbb3d3870c2e975f
Carlos Guerrero <carlosguerrero0705@gmail.com>
2024-12-02 14:53:37 -0500

    fix: Added new providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,17 @@
 // app/providers.tsx
 
+import { AvatarProvider } from "@/contexts/AvatarContext";
+import { ServerProvider } from "@/contexts/ServerContext";
 import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <main className="dark text-foreground bg-background">{children}</main>
+      <ServerProvider>
+        <AvatarProvider>
+          <main className="dark text-foreground bg-background">{children}</main>
+        </AvatarProvider>
+      </ServerProvider>
     </NextUIProvider>
   );
 }
